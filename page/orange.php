edebcaf003d4f8f7f3aafa11241dfbacc9797962
foreverwill <wangbingwen2007@gmail.com>
2013-09-25 21:23:55 +0800

    firstversion

---
+++ page/orange.php
@@ -0,0 +1,61 @@
+
+	<? //require_once('../plugin/contact-form/config.php'); ?>
+
+	<div class="layout-50 clear-fix overflow-fix">
+	
+		<div class="layout-50-left">
+		
+			<!-- <h3>On The Map</h3>
+			<div id="map"></div> -->
+			
+			<br/>
+			
+			<h3>联系我：</h3>
+			<p>
+				浙江省，杭州市；<br/>
+				西湖区，浙江大学玉泉校区，教一403。
+				<br><br>
+				--（硕士）浙江大学机械系 · 计算机图形所 <br>
+				--（本科）浙江大学信电系 · 通信工程 <br>
+				--（本科）浙大竺可桢学院 · 创新与创业管理强化班 <br><br><br>
+			</p>
+			
+			<ul class="no-list contact-list">
+				<li class="contact-list-phone"><span>手机:</span> +86 159-0660-8592</li>
+				<li class="contact-list-phone"><span>手机:</span> +86 156-9571-2625</li>
+				<!-- <li class="contact-list-fax"><span>Fax:</span> 1.800.353.253</li> -->
+				<li class="contact-list-mail"><span>邮箱:</span> foreverwill@zju.edu.cn</li>
+			</ul>
+		
+		</div>
+
+		<div class="layout-50-right">
+		
+			<h3>扫一扫，存名片：</h3>
+			<img src="image/_sample/P_card.png" width="300px" height="300px"/>
+		
+			<!-- <form name="contact" id="contact" action="" method="post">
+				
+				<div>
+				
+					<div class="form-line block">
+						<input type="text" name="contact-user-name" id="contact-user-name" value="<?=_def_data_name?>" onfocus="clearInput(this,'focus','<?=_def_data_name?>')" onblur="clearInput(this,'blur','<?=_def_data_name?>')"/>	
+					</div>
+					<div class="form-line block">
+						<input type="text" name="contact-user-email" id="contact-user-email" value="<?=_def_data_email?>" onfocus="clearInput(this,'focus','<?=_def_data_email?>')" onblur="clearInput(this,'blur','<?=_def_data_email?>')"/>	
+					</div>					
+					<div class="form-line block">
+						<textarea rows="0" cols="0" name="contact-message" id="contact-message" onfocus="clearInput(this,'focus','<?=_def_data_message?>')" onblur="clearInput(this,'blur','<?=_def_data_message?>')"><?=_def_data_message?></textarea>	
+					</div>
+					
+					<div class="form-line">
+						<a href="javascript:submitContactForm();" class="button block" id="contact-send">Send</a>
+					</div>
+				
+				</div>	
+
+			</form> -->
+		
+		</div>
+	
+	</div>
